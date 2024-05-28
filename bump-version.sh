abe3799ef0b24e7049fcc034be7c3da496f4503e
Oscar Franco <ospfranco@gmail.com>
2024-05-28 07:40:18 +0200

    Release v$(node -p "require(./package.json).version")

--- bump-version.sh
+++ bump-version.sh
@@ -6,11 +6,9 @@ npm --no-git-tag-version version patch
 
 git add .
 
-git commit -m "Bump version"
-
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
-git tag $PACKAGE_VERSION
+git tag "$PACKAGE_VERSION"
 
 git push
 
