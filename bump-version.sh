46e310394b1310348366fbbe2d676b73103af4c7
Oscar Franco <ospfranco@gmail.com>
2024-05-28 07:42:55 +0200

    0.1.7

--- bump-version.sh
+++ bump-version.sh
@@ -8,6 +8,8 @@ git add .
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
+git commit -m "$PACKAGE_VERSION"
+
 git tag "$PACKAGE_VERSION"
 
 git push
