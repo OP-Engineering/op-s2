562884e90dc87dfbf6385272b832a30dc9bf6535
Oscar Franco <ospfranco@gmail.com>
2025-05-23 19:41:53 +0200

    Update Android

--- example/android/app/src/main/java/com/opsecurestorageexample/MainApplication.kt
+++ example/android/app/src/main/java/com/opsecurestorageexample/MainApplication.kt
@@ -10,9 +10,10 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
- 
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
+
 class MainApplication : Application(), ReactApplication {
- 
+
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
@@ -20,21 +21,21 @@ class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
             }
- 
+
         override fun getJSMainModuleName(): String = "index"
- 
+
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
- 
+
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
- 
+
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
- 
+
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load(bridgelessEnabled = true)
